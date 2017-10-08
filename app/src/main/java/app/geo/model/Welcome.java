8e4bfd5f507d703a8011f2656d7254a9c5f1577d
NGrant <niall.grant1992@gmail.com>
2017-10-08 21:40:16 +0100

    First Commit

---
+++ app/src/main/java/app/geo/model/Welcome.java
@@ -0,0 +1,15 @@
+package app.geo.model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.geo.R;
+
+public class Welcome extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_welcome);
+  }
+}
