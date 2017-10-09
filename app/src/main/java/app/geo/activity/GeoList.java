2f67587d8b1fd16318d06f6c9e1b4d2373f79820
NGrant <niall.grant1992@gmail.com>
2017-10-09 23:39:38 +0100

    Created a number of blank layouts that are accessed from GeoMenu.

---
+++ app/src/main/java/app/geo/activity/GeoList.java
@@ -0,0 +1,18 @@
+package app.geo.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import app.geo.R;
+
+/**
+ * Created by niall on 09/10/17.
+ */
+
+public class GeoList extends AppCompatActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_geolist);
+  }
+}
--- app/src/main/java/app/geo/activity/GeoMenu.java
+++ app/src/main/java/app/geo/activity/GeoMenu.java
@@ -25,4 +25,34 @@ public class GeoMenu extends AppCompatActivity {
 
     startActivity(new Intent(this, Map.class));
   }
+
+  public void geoListButtonPressed(View view){
+    Toast.makeText(this, "Geo List Pressed!", Toast.LENGTH_SHORT).show();
+
+    startActivity(new Intent(this, GeoList.class));
+  }
+
+  public void myGeosButtonPressed(View view){
+    Toast.makeText(this, "My Geo Pressed!", Toast.LENGTH_SHORT).show();
+
+    startActivity(new Intent(this, MyGeos.class));
+  }
+
+  public void addGeoButtonPressed(View view){
+    Toast.makeText(this, "Add Geo Pressed!", Toast.LENGTH_SHORT).show();
+
+    startActivity(new Intent(this, AddGeo.class));
+  }
+
+  public void mailButtonPressed(View view){
+    Toast.makeText(this, "Mail Pressed!", Toast.LENGTH_SHORT).show();
+
+    startActivity(new Intent(this, Mail.class));
+  }
+
+  public void settingsButtonPressed(View view){
+    Toast.makeText(this, "Settings Pressed!", Toast.LENGTH_SHORT).show();
+
+    startActivity(new Intent(this, Settings.class));
+  }
 }
