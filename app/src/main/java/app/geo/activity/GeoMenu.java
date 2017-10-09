5ba7126aac0ccccf6eeadf908cccca2b3f19fc68
NGrant <niall.grant1992@gmail.com>
2017-10-09 23:02:24 +0100

    added activity_map.xml

--- app/src/main/java/app/geo/activity/GeoMenu.java
+++ app/src/main/java/app/geo/activity/GeoMenu.java
@@ -2,6 +2,8 @@ package app.geo.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import app.geo.R;
 
@@ -15,5 +17,9 @@ public class GeoMenu extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_geomenu);
+  }
+
+  public void mapButtonPressed(View view){
+    Toast.makeText(this, "Map Pressed!", Toast.LENGTH_SHORT).show();
   }
 }
