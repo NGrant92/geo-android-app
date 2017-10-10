3297a9457ed62f65dd63b097e98a4f920c40d256
NGrant <niall.grant1992@gmail.com>
2017-10-10 23:21:12 +0100

    Added signup.java and activity_signup.xml

---
+++ app/src/main/java/app/geo/model/Signup.java
@@ -0,0 +1,19 @@
+package app.geo.model;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import app.geo.R;
+
+/**
+ * Created by niall on 10/10/17.
+ */
+
+public class Signup extends AppCompatActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_signup);
+  }
+}
+
