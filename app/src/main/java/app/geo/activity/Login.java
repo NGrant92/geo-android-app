1356d0c310008fca8f1c6ec9453dee53ac058e59
NGrant <niall.grant1992@gmail.com>
2017-10-14 18:52:25 +0100

    Login.java/loginPressed() now verifies if login credentials match an existing User object

--- app/src/main/java/app/geo/activity/Login.java
+++ app/src/main/java/app/geo/activity/Login.java
@@ -4,8 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import app.geo.R;
+import app.geo.main.GeoApp;
 
 /**
  * Created by niall on 10/10/17.
@@ -19,7 +24,18 @@ public class Login extends AppCompatActivity {
   }
 
   public void loginPressed(View view){
-    startActivity(new Intent(this, GeoMenu.class));
+    GeoApp app = (GeoApp)getApplication();
+
+    TextView email = (TextView)findViewById(R.id.loginEmail);
+    TextView password = (TextView)findViewById(R.id.loginPassword);
+
+    if(app.validUser(email.getText().toString(), password.getText().toString())){
+      startActivity(new Intent(this, GeoMenu.class));
+    }
+    else{
+      Toast toast = Toast.makeText(this, "Invalid Credentials", Toast.LENGTH_SHORT);
+      toast.show();
+    }
   }
 }
 
