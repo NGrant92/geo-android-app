a6d629fd6855474a8bd561a838281fc812ced6ff
NGrant <niall.grant1992@gmail.com>
2017-10-14 17:39:28 +0100

    renamed signupPressed to registerPressed and it now creates a user object from field information

--- app/src/main/java/app/geo/model/Signup.java
+++ app/src/main/java/app/geo/model/Signup.java
@@ -1,9 +1,14 @@
 package app.geo.model;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import app.geo.R;
+import app.geo.activity.Login;
 
 /**
  * Created by niall on 10/10/17.
@@ -14,6 +19,18 @@ public class Signup extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_signup);
+  }
+
+  public void registerPressed(View view){
+
+    TextView firstName = (TextView)findViewById(R.id.signupFirstName);
+    TextView lastName = (TextView)findViewById(R.id.signupLastName);
+    TextView email = (TextView)findViewById(R.id.signupEmail);
+    TextView password = (TextView)findViewById(R.id.signupPassword);
+
+    User user = new User(firstName.getText().toString(), lastName.getText().toString(), email.getText().toString(), password.getText().toString());
+
+    startActivity(new Intent(this, Login.class));
   }
 }
 
