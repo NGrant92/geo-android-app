b5bf471e993f961f511c57067947c12596e78a19
NGrant <niall.grant1992@gmail.com>
2017-10-14 18:54:51 +0100

    Signup.java/registerPressed() now adds user to GeoApp.java's User object array

--- app/src/main/java/app/geo/model/Signup.java
+++ app/src/main/java/app/geo/model/Signup.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 import app.geo.R;
 import app.geo.activity.Login;
+import app.geo.main.GeoApp;
 
 /**
  * Created by niall on 10/10/17.
@@ -29,6 +30,9 @@ public class Signup extends AppCompatActivity {
     TextView password = (TextView)findViewById(R.id.signupPassword);
 
     User user = new User(firstName.getText().toString(), lastName.getText().toString(), email.getText().toString(), password.getText().toString());
+
+    GeoApp app = (GeoApp)getApplication();
+    app.newUser(user);
 
     startActivity(new Intent(this, Login.class));
   }
