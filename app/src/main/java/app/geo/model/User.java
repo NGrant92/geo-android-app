fce6f13f3211126d6db85d3a1ea19273d3f3bfae
NGrant <niall.grant1992@gmail.com>
2017-10-16 17:49:07 +0100

    adding persistence

--- app/src/main/java/app/geo/model/User.java
+++ app/src/main/java/app/geo/model/User.java
@@ -1,5 +1,8 @@
 package app.geo.model;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by niall on 10/10/17.
  * A class that will hold information of a user.
@@ -14,11 +17,36 @@ public class User {
   public String email;
   public String password;
 
+  private static final String JSON_USERID = "userId";
+  private static final String JSON_FIRSTNAME = "firstName";
+  private static final String JSON_LASTNAME = "lastName";
+  private static final String JSON_EMAIL = "email";
+  private static final String JSON_PASSWORD = "password";
+
   public User(String firstName, String lastName, String email, String password){
     this.userId = autoid++;
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
     this.password = password;
+  }
+
+  public User(JSONObject json) throws JSONException{
+    userId = json.getInt(JSON_USERID);
+    firstName = json.getString(JSON_FIRSTNAME);
+    lastName = json.getString(JSON_LASTNAME);
+    email = json.getString(JSON_EMAIL);
+    password = json.getString(JSON_PASSWORD);
+  }
+
+  public JSONObject toJSON() throws JSONException{
+    JSONObject json = new JSONObject();
+    json.put(JSON_USERID, String.valueOf(userId));
+    json.put(JSON_FIRSTNAME, firstName);
+    json.put(JSON_LASTNAME, lastName);
+    json.put(JSON_EMAIL, email);
+    json.put(JSON_PASSWORD, password);
+
+    return json;
   }
 }
