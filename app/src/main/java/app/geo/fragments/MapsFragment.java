d37d94dadf0377d3e56ca639e95aca11e88407fc
NGrant <niall.grant1992@gmail.com>
2017-10-18 16:13:33 +0100

    Added fragments/MapsFragment.java. Changed gradle build version to 2.3.3 from 3.0 after moving back to Android Studio 2.3 from Canary 3.0

---
+++ app/src/main/java/app/geo/fragments/MapsFragment.java
@@ -0,0 +1,11 @@
+package app.geo.fragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MapsFragment {
+}
