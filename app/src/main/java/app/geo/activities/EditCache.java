c98a903481f35afb5707492689190b275eb74fd8
NGrant <niall.grant1992@gmail.com>
2017-10-29 16:29:01 +0000

    Added EditCache.java and activity_edit_cache.xml

---
+++ app/src/main/java/app/geo/activities/EditCache.java
@@ -0,0 +1,17 @@
+package app.geo.activities;
+
+import android.os.Bundle;
+
+import app.geo.R;
+
+/**
+ * Created by niall on 09/10/17.
+ */
+
+public class EditCache extends Base {
+  @Override
+  protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_edit_cache);
+  }
+}
