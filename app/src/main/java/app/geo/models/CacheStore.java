32a63bd6eca45f38ba19f8ff3879b746ddff362a
NGrant <niall.grant1992@gmail.com>
2017-10-30 22:33:49 +0000

    made caches array static in CacheStore.java

--- app/src/main/java/app/geo/models/CacheStore.java
+++ app/src/main/java/app/geo/models/CacheStore.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class CacheStore {
 
-  public ArrayList<Cache> caches;
+  public static ArrayList<Cache> caches;
   private CacheSerializer serializer;
 
   public CacheStore(CacheSerializer serializer){
