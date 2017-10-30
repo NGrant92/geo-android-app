ba1104fd72144b55dfae314299ca76155e861613
NGrant <niall.grant1992@gmail.com>
2017-10-30 20:21:28 +0000

    misgrated CacheAdapter code from MyCache.java to adapters/CacheListAdapter.java

--- app/src/main/java/app/geo/activities/MyCache.java
+++ app/src/main/java/app/geo/activities/MyCache.java
@@ -16,6 +16,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.AbsListView.MultiChoiceModeListener;
+import app.geo.adapters.CacheListAdapter;
 
 import java.util.List;
 
@@ -31,7 +32,7 @@ public class MyCache extends Base implements AdapterView.OnItemClickListener{
 
   public ListView listView;
   public GeoApp app;
-  public CacheAdapter adapter;
+  public CacheListAdapter adapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState){
@@ -42,7 +43,7 @@ public class MyCache extends Base implements AdapterView.OnItemClickListener{
 
     app = (GeoApp)getApplication();
 
-    adapter = new CacheAdapter(this, app.cacheStore.caches);
+    adapter = new CacheListAdapter(this, app.cacheStore.caches);
     listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
     listView.setAdapter(adapter);
     listView.setOnItemClickListener(this);
@@ -70,41 +71,4 @@ public class MyCache extends Base implements AdapterView.OnItemClickListener{
   }
 }
 
-class CacheAdapter extends ArrayAdapter<Cache> {
-  public Context context;
-  public List<Cache> caches;
 
-  public CacheAdapter(Context context, List<Cache> caches){
-    super(context, R.layout.cache_row_layout, caches);
-    this.context = context;
-    this.caches = caches;
-  }
-
-  @Override
-  public View getView(int position, View convertView, ViewGroup parent) {
-
-    LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    ViewHolder holder;
-
-    if(convertView == null && inflater != null){
-      convertView = inflater.inflate(R.layout.cache_row_layout, parent, false);
-      holder = new ViewHolder();
-      holder.cache = caches.get(position);
-      holder.name = (TextView) convertView.findViewById(R.id.cache_name);
-      holder.location = (TextView) convertView.findViewById(R.id.cache_location);
-
-      holder.name.setText(String.valueOf(holder.cache.name));
-      holder.location.setText(String.valueOf(holder.cache.location));
-    }
-    else{
-      holder = (ViewHolder) convertView.getTag();
-    }
-    return convertView;
-  }
-
-  static class ViewHolder {
-    private Cache cache;
-    private TextView name;
-    private TextView location;
-  }
-}
--- app/src/main/java/app/geo/adapters/CacheListAdapter.java
+++ app/src/main/java/app/geo/adapters/CacheListAdapter.java
@@ -1,8 +1,72 @@
 package app.geo.adapters;
 
-/**
- * Created by niall on 30/10/17.
- */
+import app.geo.models.Cache;
+import app.geo.R;
 
-public class CacheListAdapter {
+import java.util.List;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class CacheListAdapter extends ArrayAdapter<Cache> {
+  public Context context;
+  public List<Cache> caches;
+
+  public CacheListAdapter(Context context, List<Cache> caches){
+    super(context, R.layout.cache_row_layout, caches);
+    this.context = context;
+    this.caches = caches;
+  }
+
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+
+    LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    ViewHolder holder;
+
+    if(convertView == null && inflater != null){
+      convertView = inflater.inflate(R.layout.cache_row_layout, parent, false);
+      holder = new ViewHolder();
+      holder.cache = caches.get(position);
+      holder.name = (TextView) convertView.findViewById(R.id.cache_name);
+      holder.location = (TextView) convertView.findViewById(R.id.cache_location);
+
+      holder.name.setText(String.valueOf(holder.cache.name));
+      holder.location.setText(String.valueOf(holder.cache.location));
+    }
+    else{
+      holder = (ViewHolder) convertView.getTag();
+    }
+    return convertView;
+  }
+
+  static class ViewHolder {
+    private Cache cache;
+    private TextView name;
+    private TextView location;
+  }
+
+  public int getCount(){
+    return caches.size();
+  }
+
+  @Override
+  public Cache getItem(int position) {
+    return caches.get(position);
+  }
+
+  @Override
+  public long getItemId(int position) {
+    //return coffeeList.get(position).coffeeId;
+    return position;
+  }
+
+  @Override
+  public int getPosition(Cache c) {
+    return caches.indexOf(c);
+  }
 }
