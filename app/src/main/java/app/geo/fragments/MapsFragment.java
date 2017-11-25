7f6ed964c37ad9821c7cba6a4bc1b4275abf0fe5
NGrant <niall.grant1992@gmail.com>
2017-11-25 18:39:31 +0000

    MapsFragment.java now asks user to turn on location services if location is turned off when fragment is loaded

--- app/src/main/java/app/geo/fragments/MapsFragment.java
+++ app/src/main/java/app/geo/fragments/MapsFragment.java
@@ -4,6 +4,9 @@ package app.geo.fragments;
 import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
@@ -23,11 +26,18 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -46,6 +56,7 @@ import app.geo.R;
 
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 import static android.Manifest.permission.CAMERA;
+import static com.android.volley.VolleyLog.TAG;
 
 /**
  * @author Niall Grant 05/11/2017
@@ -76,6 +87,7 @@ public class MapsFragment extends MapFragment implements
   GeoApp app = GeoApp.getInstance();
 
   private static final int PERMISSION_REQUEST_CODE = 200;
+  protected static final int REQUEST_CHECK_SETTINGS = 0x1;
 
   private final int[] MAP_TYPES = {
       GoogleMap.MAP_TYPE_SATELLITE,
@@ -84,6 +96,11 @@ public class MapsFragment extends MapFragment implements
       GoogleMap.MAP_TYPE_TERRAIN,
       GoogleMap.MAP_TYPE_NONE
   };
+
+  @Override
+  public boolean onMarkerClick(Marker marker) {
+    return false;
+  }
 
   public interface OnFragmentInteractionListener {
     void toggle(View v);
@@ -107,6 +124,7 @@ public class MapsFragment extends MapFragment implements
       mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
       createLocationCallback();
       createLocationRequest();
+      displayLocationSettingsRequest();
     }
     catch(SecurityException se) {
       Toast.makeText(getActivity(),"Check Your Permissions",Toast.LENGTH_SHORT).show();
@@ -316,8 +334,44 @@ public class MapsFragment extends MapFragment implements
 
   }
 
-  @Override
-  public boolean onMarkerClick(Marker marker) {
-    return false;
+  //REF: https://stackoverflow.com/a/33254073
+  private void displayLocationSettingsRequest() {
+    GoogleApiClient googleApiClient = new GoogleApiClient.Builder(getActivity()).addApi(LocationServices.API).build();
+    googleApiClient.connect();
+
+    LocationRequest locationRequest = LocationRequest.create();
+    locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    locationRequest.setInterval(10000);
+    locationRequest.setFastestInterval(10000 / 2);
+
+    LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+    builder.setAlwaysShow(true);
+
+    PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+    result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+      @Override
+      public void onResult(LocationSettingsResult result) {
+        final Status status = result.getStatus();
+        switch (status.getStatusCode()) {
+          case LocationSettingsStatusCodes.SUCCESS:
+            Log.i(TAG, "All location settings are satisfied.");
+            break;
+          case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+            Log.i(TAG, "Location settings are not satisfied. Show the user a dialog to upgrade location settings ");
+
+            try {
+              // Show the dialog by calling startResolutionForResult(), and check the result
+              // in onActivityResult().
+              status.startResolutionForResult(getActivity(), REQUEST_CHECK_SETTINGS);
+            } catch (IntentSender.SendIntentException e) {
+              Log.i(TAG, "PendingIntent unable to execute request.");
+            }
+            break;
+          case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+            Log.i(TAG, "Location settings are inadequate, and cannot be fixed here. Dialog not created.");
+            break;
+        }
+      }
+    });
   }
 }
