333fe83193a41b4f224217380c225852f2525a4c
NGrant <niall.grant1992@gmail.com>
2017-11-26 14:24:04 +0000

    added fragments/SearchFragment.java

---
+++ app/src/main/java/app/geo/fragments/SearchFragment.java
@@ -0,0 +1,31 @@
+package app.geo.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by niall on 26/11/17.
+ */
+
+public class SearchFragment extends CachesFragment {
+  public SearchFragment() {}
+
+  public static SearchFragment newInstance() {
+    return new SearchFragment();
+  }
+
+  @Override
+  public void onAttach(Context c) {
+    super.onAttach(c);
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+  }
+}
