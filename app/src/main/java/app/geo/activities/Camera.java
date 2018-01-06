2c1f4d1a47070a7d7a59f80de3a7afae9b41db04
NGrant <niall.grant1992@gmail.com>
2018-01-06 16:48:28 +0200

    added reference to items from activity_photo.xml in activities/Camera.java

--- app/src/main/java/app/geo/activities/Camera.java
+++ app/src/main/java/app/geo/activities/Camera.java
@@ -5,16 +5,28 @@ import app.geo.R;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View.OnClickListener;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class Camera extends AppCompatActivity
+public class Camera extends AppCompatActivity implements OnClickListener
 {
+
+  private Button savePhoto;
+  private Button takePhoto;
+  private ImageView cacheImage;
 
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_photo);
+
+    cacheImage = (ImageView) findViewById(R.id.residenceImage);
+    takePhoto = (Button)findViewById(R.id.takePhoto);
+    savePhoto = (Button)findViewById(R.id.savePhoto);
+    savePhoto.setEnabled(false);
 
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
   }
