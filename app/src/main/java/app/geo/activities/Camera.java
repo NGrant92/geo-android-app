7d8201e5a94edad91c9038f86b2a81ba0ee5835d
NGrant <niall.grant1992@gmail.com>
2018-01-06 17:03:42 +0200

    setOnClickListeners() to save and take photo buttons

--- app/src/main/java/app/geo/activities/Camera.java
+++ app/src/main/java/app/geo/activities/Camera.java
@@ -5,10 +5,18 @@ import app.geo.R;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.View.OnClickListener;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
+
+/**
+ * @author Niall Grant 05/11/2017
+ * An activity class used to take and save a photo of the cache object
+ *
+ * Main reference source: https://wit-hdip-computer-science.github.io/semester-2-mobile-app-dev/topic11-a/book-a-myrent-11%20(Camera)/index.html
+ */
 
 public class Camera extends AppCompatActivity implements OnClickListener
 {
@@ -28,7 +36,17 @@ public class Camera extends AppCompatActivity implements OnClickListener
     savePhoto = (Button)findViewById(R.id.savePhoto);
     savePhoto.setEnabled(false);
 
+    savePhoto.setOnClickListener(this);
+    takePhoto.setOnClickListener(this);
+
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+  }
+
+  @Override
+  public void onClick(View v)
+  {
+    // TODO Auto-generated method stub
+
   }
 
   @Override
